5591bd507121adf73276dfd72d2ee92e29aed987
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-08-09 23:47:07 +0900

    test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,56 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("tt");
-        System.out.println("cc");
-        System.out.println("dd");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");        System.out.println("ee");
-        System.out.println("ee");
 
-        System.out.println("ee");
-        System.out.println("ee");
-
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-        System.out.println("ee");
-
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-        System.out.println("qq");
-
-        System.out.println("qq");
-
-        System.out.println("qq");
-        System.out.println("qq");
     }
 }
